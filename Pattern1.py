a59f3be55a5582538fbae7b4b082d9abd3f9d3c8
Subham Bhuyan <subham@suvarchalasolutionzs.com>
2023-07-17 20:57:05 +0530

    p1
    added one pattern

---
+++ Pattern1.py
@@ -0,0 +1,15 @@
+"""
+Pattern 1
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+(n times)
+"""
+
+n = input("Enter the number of rows you want to print.\n")
+for i in range(int(n)):
+    for j in range(i+1):
+        print("* ", end="")
+    print("\r")
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # All-Pattern-In-Python
 This repo will contain must patterns , how we can create those using programming skills.
+
+Pattern 1: 
+```
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+```
