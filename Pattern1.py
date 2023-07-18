578ef06d3fcd57663ac3afe3cd0d8afd0b3e8876
Subham Bhuyan <subham@suvarchalasolutionzs.com>
2023-07-18 17:08:28 +0530

    added new pattern

--- Pattern1.py
+++ Pattern1.py
@@ -10,6 +10,6 @@ Pattern 1
 
 n = input("Enter the number of rows you want to print.\n")
 for i in range(int(n)):
-    for j in range(i+1):
+    for j in range(i + 1):
         print("* ", end="")
     print("\r")
